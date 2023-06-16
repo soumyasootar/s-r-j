4874271c1499d466f91965e4fa0c9955688d214c
Narayan2208 <narayandutta2208@gmail.com>
2023-06-16 21:55:00 +0530

    explore page

--- src/Pages/Explore.jsx
+++ src/Pages/Explore.jsx
@@ -1,15 +1,10 @@
-import { Box, Button } from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react'
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 const Explore = () => {
   return (
-    <Box height={"100vh"} >
-{/* Grid of Blogs  */}
-
-<Link to="/explore/1234">
-<Button>OPEN BLOG</Button>
-</Link>
+    <Box height={"100vh"}>
+        iaifhiow
     </Box>
   )
 }
