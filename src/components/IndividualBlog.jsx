34712715a77eefa47ac7aa923faf29ff33afeefb
Narayan2208 <narayandutta2208@gmail.com>
2023-06-25 17:01:38 +0530

    individual page data fetch complete

--- src/components/IndividualBlog.jsx
+++ src/components/IndividualBlog.jsx
@@ -1,10 +1,4 @@
-import {
-  Box,
-  Flex,
-  Heading,
- 
-  Text,
-} from "@chakra-ui/react";
+import { Box, Flex, Heading, Text } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
@@ -12,31 +6,34 @@ import { useParams } from "react-router-dom";
 import "./individualBlog.css";
 import axios from "axios";
 import parse from "html-react-parser";
-<link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300&display=swap" rel="stylesheet"></link>
+<link
+  href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300&display=swap"
+  rel="stylesheet"
+></link>;
 
 const IndividualBlog = () => {
   let { id } = useParams();
   let [state, setState] = useState({
-    images : [],
-    title : "", 
-    content : "",
-    author : "",
-    createdAt : ""
-  })
-  useEffect(()=>{
-    fetchSingleBlog()
-  },[])
-  const fetchSingleBlog = ()=>{
+    images: [],
+    title: "",
+    content: "",
+    author: "",
+    createdAt: "",
+  });
+  useEffect(() => {
+    fetchSingleBlog();
+  }, []);
+  const fetchSingleBlog = () => {
     axios
-    .get(`http://localhost:3001/blog/posts/${id}`)
-    .then((response) => {
-      setState(response.data)
-      console.log(response.data, "responceData");;
-    })
-    .catch((error) => {
-      console.error("Error fetching data:", error);
-    });
-  }
+      .get(`http://localhost:3001/blog/posts/${id}`)
+      .then((response) => {
+        setState(response.data);
+        console.log(response.data, "responceData");
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  };
   const formatCreatedAt = (createdAt) => {
     const date = new Date(createdAt);
     const formattedDate = date.toLocaleString("en-US", {
@@ -52,7 +49,7 @@ const IndividualBlog = () => {
   console.log(id, "individualID");
   console.log(id);
   return (
-    <Box h={{sm: "100vh", md: "100vh", lg: "auto" }}>
+    <Box h={{ sm: "100vh", md: "100vh", lg: "auto" }}>
       <Box
         bg={"white"}
         borderRadius={"xl"}
@@ -60,38 +57,33 @@ const IndividualBlog = () => {
         margin={"auto"}
         my="100px"
         width={["90%", "80%"]} // Adjust the width for mobile and desktop views
-             >
-        <Carousel  showThumbs={false} showStatus={false} autoPlay={true} interval={2000}  infiniteLoop={true}>
-          <div>
-            <img src={state.images[0]} alt="img"/>
-          </div>
-          <div>
-            <img src={state.images[1]} alt="img"/>
-          </div>
-          <div>
-            <img src={state.images[2]} alt="img"/>
-          </div>
-          <div>
-            <img src={state.images[3]} alt="img"/>
-          </div>
-          <div>
-            <img src={state.images[4]} alt="img"/>
-          </div>
+      >
+        <Carousel
+          showThumbs={false}
+          showStatus={false}
+          autoPlay={true}
+          interval={2000}
+          infiniteLoop={true}
+        >
+          {state.images.map((image, index) => (
+            <div key={index}>
+              <img src={image} alt="img" />
+            </div>
+          ))}
         </Carousel>
-        <Heading fontFamily={ "Comfortaa, cursive"} my="30"> {state.title} </Heading>
-        <Text>
-        {
-          parse(state.content)
-        }
-        </Text>
-      <Flex mt={5} justifyContent={"space-between"}>
-        <Box>
-          <Text>Author : {state.author}</Text>
-        </Box>
-        <Box>
-          <Text>CreatedAt : {formatCreatedAt(state.createdAt)}</Text>
-        </Box>
-      </Flex>
+        <Heading fontFamily={"Comfortaa, cursive"} my="30">
+          <span className="first-letter">{state.title.charAt(0)}</span>
+          {state.title.slice(1)}
+        </Heading>
+        <Text fontFamily={"'Salsa', cursive"}>{parse(state.content)}</Text>
+        <Flex mt={5} justifyContent={"space-between"}>
+          <Box>
+            <Text fontFamily={"sans-serif"}>Author : {state.author}</Text>
+          </Box>
+          <Box>
+            <Text fontFamily={"sans-serif"}>CreatedAt : {formatCreatedAt(state.createdAt)}</Text>
+          </Box>
+        </Flex>
       </Box>
     </Box>
   );
